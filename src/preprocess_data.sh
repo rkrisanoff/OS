11eceee73280f526117fe19e9f84da4897058c32
drukhary <rkrisanoff@gmail.com>
2022-09-23 12:21:24 +0300

    fixed preprocessing

--- src/preprocess_data.sh
+++ src/preprocess_data.sh
@@ -2,10 +2,17 @@
 
 rawResultDir="data/raw"
 proccessedResultDir="data/processed"
-numberOfMeasurements=100
+numberOfMeasurements=1000
 
 cat "$rawResultDir"/cpu.log  | tail -n $(("$numberOfMeasurements"+2)) | head -n $(("$numberOfMeasurements"+1)) > "$proccessedResultDir"/cpu.log
 
-cat "$rawResultDir"/io.log  | tail -n $(("$numberOfMeasurements"+2)) | head -n $(("$numberOfMeasurements"+1)) > "$proccessedResultDir"/io.log
+echo "11:11:09 AM   UID       PID   kB_rd/s   kB_wr/s kB_ccwr/s iodelay  Command" > "$proccessedResultDir"/io.log
+
+
+cat "$rawResultDir"/io.log  | tail -n $(("$numberOfMeasurements"+2)) | head -n $(("$numberOfMeasurements"+1)) >> "$proccessedResultDir"/io.log
 
 cp "$rawResultDir"/network.log "$proccessedResultDir"/network.log
+
+echo "total running sleeping stopped zombie" >"$proccessedResultDir"/threads.log
+
+cat "$rawResultDir"/threads.log | grep Threads | awk '{print $2,$4,$6,$8,$10}' >> "$proccessedResultDir"/threads.log
