808109122a76ca980ca0ca4ea3125787d68cc025
drukhary <rkrisanoff@gmail.com>
2022-10-07 02:23:17 +0300

    small fixes

--- src/analysis.sh
+++ src/analysis.sh
@@ -6,28 +6,29 @@ work_dir_flag=''
 work_dir=''
 number_of_measurements_flag=''
 number_of_measurements=''
-flameOpts=''
 flame_graph_flag=''
 flame_graph_dir=''
 
 while getopts 't:d:n:f:h' flag; do
     case "${flag}" in
-    h) echo "Usage: analysis
+    h)
+        echo "Usage: analysis
                 -t path to the target program
                 -d path to the working directory
                 -n numbers of measurements (in seconds)
                 -f path to the FlameGraph directory"
-                exit
-    ;;
+        exit
+        ;;
     t)
         target_flag='true'
         target_path="${OPTARG}"
         if [ -f "${target_path}" ]; then
             target=$(readlink -f "${target_path}")
-            echo "the target of perfomance researching is " "${target}"
+            target_name=$(basename "${target_path}")
+            echo "the target of perfomance researching is " "${target_name}"
         else
             echo "invalid target pathfile"
-            exit
+            exit 1
         fi
         ;;
     d)
@@ -38,7 +39,7 @@ while getopts 't:d:n:f:h' flag; do
             echo "the working directory is" "${work_dir}"
         else
             echo "invalid working directory path"
-            exit
+            exit 1
         fi
         ;;
     n)
@@ -48,7 +49,7 @@ while getopts 't:d:n:f:h' flag; do
             echo "number of measurements" "${number_of_measurements}" "is correct"
         else
             echo "number of measurements" "${number_of_measurements}" "is uncorrect and must be between 0 and 180"
-            exit
+            exit 1
         fi
         ;;
     f)
@@ -63,7 +64,11 @@ while getopts 't:d:n:f:h' flag; do
         fi
         ;;
     *)
-        print_usage
+        echo "Usage: analysis
+                -t path to the target program
+                -d path to the working directory
+                -n numbers of measurements (in seconds)
+                -f path to the FlameGraph directory"
         exit 1
         ;;
     esac
@@ -105,6 +110,7 @@ cd "${work_dir}" || exit
 PID=$(pidof "${target}")
 
 # collecting data
+lsof -p "${PID}" >>"${work_dir}"/data/raw/file_networks_list.log
 ps huH -p "${PID}" | wc >"${work_dir}"/data/raw/threads_count.log &
 pmap -x "${PID}" >"${work_dir}"/data/raw/memory_map.log &
 top -b -H -n$(("${number_of_measurements}" / 3)) -p "${PID}" >"${work_dir}"/data/raw/threads.log &
@@ -112,9 +118,8 @@ pidstat -p "${PID}" 1 "${number_of_measurements}" >"${work_dir}"/data/raw/cpu.lo
 pidstat -p "${PID}" -d 1 "${number_of_measurements}" >"${work_dir}"/data/raw/io.log &
 if [ "${flame_graph_path_flag}" = 'true' ]; then
     perf record -F 99 -p "${PID}" -g -- sleep "${number_of_measurements}"
-    perf script >"${work_dir}"/data/raw/out.perf & 
+    perf script >"${work_dir}"/data/raw/out.perf &
     echo "flame graph will be"
-    
 fi
 
 echo "rx tx" >"${work_dir}"/data/raw/network.log
@@ -122,9 +127,7 @@ bmon -r 1 -p wlan0 -o format:fmt='$(attr:rxrate:bytes) $(attr:txrate:bytes)\n' >
 for network_port in $(sudo netstat -ltup | grep "${PID}" | awk '{split($4,port_parts,":"); print port_parts[2]}' | grep '^[0-9][0-9]*$'); do
     sudo tcpdump -i any port "${network_port}" | cat >"${work_dir}"/data/raw/networks/"${network_port}".log &
 done
-lsof -p "${PID}" >>"${work_dir}"/data/raw/file_networks_list.log
 sleep "${number_of_measurements}" && sudo killall bmon && sudo killall tcpdump >/dev/null
-
 
 sudo killall "${target}" >/dev/null
 
@@ -134,7 +137,7 @@ tail -n $(("$number_of_measurements" + 2)) <"${work_dir}"/data/raw/io.log | head
 if [ "${flame_graph_path_flag}" = 'true' ]; then
     "${flame_graph_dir}"/stackcollapse-perf.pl "${work_dir}"/data/raw/out.perf >"${work_dir}"/data/processed/out.folded
     "${flame_graph_dir}"/flamegraph.pl --title 180188 --width 1920 --height 24 "${work_dir}"/data/processed/out.folded >"${work_dir}"/data/results/kernel.svg
-fi 
+fi
 cp "${work_dir}"/data/raw/network.log "${work_dir}"/data/processed/network.log
 echo "PID     USER      PR   NI VIRT     RES    SHR  S  %CPU  %MEM   TIME+ COMMAND" >"${work_dir}"/data/processed/threads.log
-grep "${PID}" <"${work_dir}"/data/raw/threads.log >>"${work_dir}"/data/processed/threads.log
+grep "${target_name}" <"${work_dir}"/data/raw/threads.log >>"${work_dir}"/data/processed/threads.log
